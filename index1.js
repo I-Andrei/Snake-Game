9ac496dae037681a9c2805b54e370ba4518d3860
I-Andrei <78320950+I-Andrei@users.noreply.github.com>
2022-07-02 15:13:21 +0300

    Changed to matrix and objects

--- index1.js
+++ index1.js
@@ -1,71 +1,68 @@
 let squares = ""
-foodPos = 154
-let snakeBody = [142, 143, 144]
+foodPos = [ {x: 14, y: 14} ]
+let snakeBody = [
+{x: 14, y: 2}, 
+{x: 14, y: 3}, 
+{x: 14, y: 4} ]
 let eatFood = 0, score = 0, maxScore = 0
-let endgame = 0, row = 1, column = 1, snakeHeadColumn = 0
+let endgame = 0, line = 1, column = 1, snakeFound = 0
 
 function gridSquares() {
   squares = ""
   if (direction == "up")
-    snakeBody.push(snakeBody[snakeBody.length - 1] - 20)
+    snakeBody.push({x: snakeBody[snakeBody.length - 1].x - 1, y: snakeBody[snakeBody.length - 1].y})
   else if (direction == "down")
-    snakeBody.push(snakeBody[snakeBody.length - 1] + 20)
+    snakeBody.push({x: snakeBody[snakeBody.length - 1].x + 1, y: snakeBody[snakeBody.length - 1].y})
   else if (direction == "left")
-    snakeBody.push(snakeBody[snakeBody.length - 1] - 1)
+    snakeBody.push({x: snakeBody[snakeBody.length - 1].x, y: snakeBody[snakeBody.length - 1].y - 1})
   else if (direction == "right")
-    snakeBody.push(snakeBody[snakeBody.length - 1] + 1)
-  if (direction !== "" && snakeBody[snakeBody.length - 2] !== eatFood){
+    snakeBody.push({x: snakeBody[snakeBody.length - 1].x, y: snakeBody[snakeBody.length - 1].y + 1})
+  
+  if (snakeBody[snakeBody.length - 1].x == foodPos[0].x && snakeBody[snakeBody.length - 1].y == foodPos[0].y){
+    eatFood = 1
+    foodPos[0].x = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+    foodPos[0].y = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+    ++score
+    document.getElementById("score").innerHTML = "Score: " + score
+  }
+
+  if (direction !== "" && eatFood == 0){
     snakeBody.splice(0, 1)
     eatFood = 0
   }
-  for (let i = 1; i <= 400; ++i){
-    let checkSnakeBody = 0
-    for (let x = 0; x < snakeBody.length; ++x){
-      if (i == snakeBody[x]){
-        squares += '<div class=snakecolor id=' + row + '/' + column + '></div>'
-        ++checkSnakeBody
-        if (x = snakeBody.length - 1)
-          snakeHeadColumn = column
-      }
+  
+  for (line = 1; line <= 20; ++line)
+    for (column = 1; column <= 20; ++column){
+      for (let j = 0; j < snakeBody.length; ++j)
+        if (line == snakeBody[j].x && column == snakeBody[j].y)
+          snakeFound = 1
+      if (snakeFound == 1){
+        squares += '<div class=snakecolor></div>'
+        snakeFound = 0
+      } else if (line == foodPos[0].x && column == foodPos[0].y)
+        squares += '<div class=food></div>'
+      else
+        squares += '<div class=cell-grid></div>'
     }
-    if (snakeBody[snakeBody.length - 1] == foodPos){
-      eatFood = foodPos
-      foodPos = Math.floor(Math.random() * 400);
-      ++score
-      document.getElementById("score").innerHTML = "Score: " + score
-    }
-    if (i == foodPos){
-      squares += '<div class=food id=' + row + '/' + column + '></div>'
-    }  
-    else if (checkSnakeBody !== 1){
-      squares += '<div class=cell-grid id=' + row + '/' + column + '></div>'
-    }
-    if (snakeHeadColumn == 20 && direction == "left")
-      ++endgame
-    checkSnakeBody = 0
-    if (i % 20 == 0){
-      ++row
-      column = 0
-      if (row == 21)
-        row = 1
-    }
-    ++column
-  }
+  eatFood = 0
   document.getElementById("snakeGrid").innerHTML = squares
-  if (snakeHeadColumn == 1 && direction == "right")
+
+  if ((snakeBody[snakeBody.length - 1].x == 0 && direction == "up") || (snakeBody[snakeBody.length - 1].x == 21 && direction == "down") || (snakeBody[snakeBody.length - 1].y == 0 && direction == "left") || (snakeBody[snakeBody.length - 1].y == 21 && direction == "right"))
     ++endgame
-  if (snakeBody[snakeBody.length - 1] < 1 || snakeBody[snakeBody.length - 1] > 400)
-    ++endgame
-  for (let x = 0; x < snakeBody.length - 1; ++x)
-      if (snakeBody[snakeBody.length - 1] == snakeBody[x])
-        ++endgame
+  for (let i = 0; i < snakeBody.length - 3; ++i)
+    if (snakeBody[snakeBody.length - 1].x == snakeBody[i].x && snakeBody[snakeBody.length - 1].y == snakeBody[i].y)
+      ++endgame
+  
   if(endgame > 0){
-    snakeBody = [142, 143, 144]
+    snakeBody = [
+      {x: 14, y: 2}, 
+      {x: 14, y: 3}, 
+      {x: 14, y: 4} ]
+    foodPos = [ {x: 14, y: 14} ]
     direction = ""
     if(maxScore < score)
       maxScore = score
     score = 0
-    snakeHeadColumn = 0
     document.getElementById("maxScore").innerHTML = "Max Score: " + maxScore
     document.getElementById("score").innerHTML = "Score"
     document.getElementById("snakeGrid").innerHTML = "You Lost! Try Again!"
@@ -79,28 +76,22 @@ let direction = ""
 document.onkeydown = function (e) {
   switch (e.keyCode) {
     case 37:
-      if (snakeBody[snakeBody.length - 1] - 1 !== snakeBody[snakeBody.length - 2])
+      if (snakeBody[snakeBody.length - 1].y - 1 !== snakeBody[snakeBody.length - 2].y)
         direction = "left"
       break;
     case 38:
-      if (snakeBody[snakeBody.length - 1] - 20 !== snakeBody[snakeBody.length - 2])
+      if (snakeBody[snakeBody.length - 1].x - 1 !== snakeBody[snakeBody.length - 2].x)
         direction = "up"
       break;
     case 39:
-      if (snakeBody[snakeBody.length - 1] + 1 !== snakeBody[snakeBody.length - 2])
+      if (snakeBody[snakeBody.length - 1].y + 1 !== snakeBody[snakeBody.length - 2].y)
         direction = "right"
       break;
     case 40:
-      if (snakeBody[snakeBody.length - 1] + 20 !== snakeBody[snakeBody.length - 2])
+      if (snakeBody[snakeBody.length - 1].x + 1 !== snakeBody[snakeBody.length - 2].x)
         direction = "down"
       break;
     }
 };
 
 setInterval(gridSquares, 200, direction);
-
-
-
-
-
-
